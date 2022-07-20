95bd8926a1653e47141e19f833d97322ec53bd57
Renaud Lazardeux <Renaud.lazardeux@gmail.com>
2022-07-20 15:33:00 +0200

    ajout trophées

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,10 @@
 import { clubList } from '../datas/ClubList'
 import '../styles/Card.css'
+import L1 from '../assets/championnat.png'
+import CDF from '../assets/coupeDeFrance.png'
+import CDL from '../assets/coupeDeLigue.png'
+import TDC from '../assets/tropheeDesChamp.png'
+
 
 function CLubs() {
 
@@ -11,21 +16,39 @@ function CLubs() {
                     <a href='#' key={club.name} className='ligue1-club-name'>
                         <div>
                             <h2>{club.name}</h2>
-                            <img src={club.logo} alt='ligue1 logo club' className='ligue1-logo-club' />
+                            <br />
+                            <img src={club.logo} alt='ligue1 logo club' className='logo-club' />
+                            <br />
                         </div>
                         <p>Stade : {club.stadium}</p>
                         <p>Année de création : {club.year}</p>
-                        <span>
+                        <br />
+                        <span className='palmares'>
                             <h3>Palmares:</h3>
-                            <p>L1:</p>
-                            <p>Cdf:</p>
-                            <p>Cdl:</p>
-                            <p>Tdc:</p>
+                            <br />                             <br />
+
+                            <p>
+                                <img src={L1} alt='ligue1' className='logo-trophee' />
+
+                                Ligue 1: {club.l1}
+                            </p>
+                            <p>
+                                <img src={CDF} alt='coupeFrance' className='logo-trophee' />
+                                Coupe de france: {club.cdf}
+                            </p>
+                            <p>
+                                <img src={CDL} alt='coupeLigue' className='logo-trophee' />
+                                Coupe de la ligue: {club.cdl}
+                            </p>
+                            <p>
+                                <img src={TDC} alt='tropheeChamp' className='logo-trophee' />
+                                Trophée des champions: {club.tdc}
+                            </p>
                         </span>
                     </a>
                 ))}
             </div>
-        </div>
+        </div >
     )
 
 
--- src/datas/ClubList.js
+++ src/datas/ClubList.js
@@ -24,120 +24,200 @@ export const clubList = [
         name: 'Ajaccio',
         stadium: 'Stade François-Coty',
         year: '1910',
-        logo: ajaccio
+        logo: ajaccio,
+        l1: 0,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Angers',
         stadium: 'Stade Raymond-Kopa',
         year: '1919',
-        logo: angers
+        logo: angers,
+        l1: 0,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Auxerre',
         stadium: 'Stade de l\'Abbé-Deschamps',
         year: '1905',
-        logo: auxerre
+        logo: auxerre,
+        l1: 1,
+        cdf: 4,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Brest',
         stadium: 'Stade Francis-Le Blé',
         year: '1950',
-        logo: brest
+        logo: brest,
+        l1: 0,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Clermont',
         stadium: 'Stade Gabriel-Montpied',
         year: '1990',
-        logo: clermont
+        logo: clermont,
+        l1: 0,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Lens',
         stadium: 'Stade Bollaert-Delelis',
         year: '1906',
-        logo: lens
+        logo: lens,
+        l1: 1,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Lille',
         stadium: 'Décathlon Arena-Stade Pierre-Mauroy',
         year: '1944',
-        logo: lille
+        logo: lille,
+        l1: 4,
+        cdf: 6,
+        cdl: 0,
+        tdc: 1
     },
     {
         name: 'Lorient',
         stadium: 'Stade du Moustoir',
         year: '1926',
-        logo: lorient
+        logo: lorient,
+        l1: 0,
+        cdf: 1,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Lyon',
         stadium: 'Groupama Stadium',
         year: '1950',
-        logo: lyon
+        logo: lyon,
+        l1: 7,
+        cdf: 5,
+        cdl: 1,
+        tdc: 7
     },
     {
         name: 'Monaco',
         stadium: 'Stade Louis-II',
         year: '1924',
-        logo: monaco
+        logo: monaco,
+        l1: 8,
+        cdf: 5,
+        cdl: 1,
+        tdc: 4
     },
     {
         name: 'Montpellier',
         stadium: 'Stade de la Mosson',
         year: '1919',
-        logo: montpellier
+        logo: montpellier,
+        l1: 1,
+        cdf: 1,
+        cdl: 1,
+        tdc: 0
     },
     {
         name: 'Nantes',
         stadium: 'Stade de la Beaujoire - Louis Fonteneau2',
         year: '1943',
-        logo: nantes
+        logo: nantes,
+        l1: 8,
+        cdf: 3,
+        cdl: 0,
+        tdc: 2
     },
     {
         name: 'Nice',
         stadium: 'Allianz Riviera',
         year: '1932',
-        logo: nice
+        logo: nice,
+        l1: 4,
+        cdf: 3,
+        cdl: 0,
+        tdc: 1
     },
     {
         name: 'Marseille',
         stadium: 'Orange Vélodrome',
         year: '1899',
-        logo: marseille
+        logo: marseille,
+        l1: 9,
+        cdf: 10,
+        cdl: 3,
+        tdc: 3
     },
     {
         name: 'Paris',
         stadium: 'Parc des Princes',
         year: '1970',
-        logo: paris
+        logo: paris,
+        l1: 10,
+        cdf: 14,
+        cdl: 9,
+        tdc: 10
     },
     {
         name: 'Reims',
         stadium: 'Stade Auguste-Delaune',
         year: '1931',
-        logo: reims
+        logo: reims,
+        l1: 6,
+        cdf: 2,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Rennes',
         stadium: 'Roazhon Park',
         year: '1901',
-        logo: rennes
+        logo: rennes,
+        l1: 0,
+        cdf: 3,
+        cdl: 0,
+        tdc: 1
     },
     {
         name: 'Strasbourg',
         stadium: 'Stade de la Meinau',
         year: '1906',
-        logo: strasbourg
+        logo: strasbourg,
+        l1: 1,
+        cdf: 3,
+        cdl: 3,
+        tdc: 0
     },
     {
         name: 'Toulouse',
         stadium: 'Stadium de Toulouse',
         year: '1970',
-        logo: toulouse
+        logo: toulouse,
+        l1: 0,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     },
     {
         name: 'Troyes',
         stadium: 'Stade de l\'Aube',
         year: '1986',
-        logo: troyes
+        logo: troyes,
+        l1: 0,
+        cdf: 0,
+        cdl: 0,
+        tdc: 0
     }
 ]
